da7e38e34e6eeca05f2f258fb86c147a13a6402c
Jacob Bieker <jacob@bieker.tech>
2023-03-03 16:27:39 +0000

    Add test of output

--- tests/model/test_idam.py
+++ tests/model/test_idam.py
@@ -26,3 +26,14 @@ def test_irradience_forecastor_pv_forward():
     pv_tensor = torch.rand((2, 2, 128, 128))
     output_tensor = model(input_tensor, pv_tensor, False)
     assert not torch.any(torch.isnan(output_tensor))
+
+def test_irradience_forecastor_pv_backward():
+    model = PsuedoIrradienceForecastor()
+    input_tensor = torch.rand((2, 3, 12, 128, 128))
+    pv_tensor = torch.rand((2, 2, 128, 128))
+    target_tensor = torch.rand((2,1,128,128))
+    output_tensor = model(input_tensor, pv_tensor, False)
+    loss = F.mse_loss(output_tensor, target_tensor)
+    assert not torch.any(torch.isnan(loss))
+    loss.backward()
+
