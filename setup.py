db22fc13ef02e77ef04c532454e83a1fb9e83a10
BumpVersion Action <bumpversion@github-actions>
2023-05-02 12:49:44 +0000

    Bump version: 0.0.7 → 0.0.8

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ install_requires = (this_directory / "requirements.txt").read_text().splitlines(
 
 setup(
     name="pseudo_labeller",
-    version="0.0.7",
+    version="0.0.8",
     license="MIT",
     description="Psuedo PV/Irradience Labeller",
     author="Jacob Bieker",
